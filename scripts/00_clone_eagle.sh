3ce102c000dcbf6a687248ea50254c3b8fcfbca4
xxxhfqq <2731897659@qq.com>
2026-06-07 16:59:07 +0800

    Use accelerated public URL for EAGLE clone

--- scripts/00_clone_eagle.sh
+++ scripts/00_clone_eagle.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 
 mkdir -p third_party
 
+EAGLE_REPO_URL="${EAGLE_REPO_URL:-https://gh-proxy.com/https://github.com/SafeAILab/EAGLE.git}"
+
 if [ ! -d "third_party/EAGLE/.git" ]; then
-  git clone https://github.com/SafeAILab/EAGLE.git third_party/EAGLE
+  git clone --depth=1 "$EAGLE_REPO_URL" third_party/EAGLE
 else
   echo "third_party/EAGLE already exists, skip clone."
 fi
